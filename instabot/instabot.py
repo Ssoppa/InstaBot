836e5ec69019f821e6e87ccab2346ce60216d9b3
Matheus Soppa Geremias <suppersoppa@gmail.com>
2023-05-03 10:33:15 -0300

    declared the unit tests for the scheduler

---
+++ instabot/instabot.py
@@ -0,0 +1,11 @@
+class Scheduler:
+    def __init__(self) -> None:
+        pass
+
+class Poster:
+    def __init__(self) -> None:
+        pass
+
+class Verifier:
+    def __init__(self) -> None:
+        pass
