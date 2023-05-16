8a939de0300cdb886dd3e3d522a793236d709680
Matheus Soppa Geremias <suppersoppa@gmail.com>
2023-05-16 19:44:57 -0300

    created Poster functions

--- instabot/poster.py
+++ instabot/poster.py
@@ -13,17 +13,100 @@ from instabot.post import Post
 
 
 class Poster:
-    def __init__(self) -> None:
+    def __init__(self, *args, **kwargs) -> None:
+        self._username = kwargs.get('username', None)
+        self._account_name = kwargs.get('account_name', None)
+        self._password = kwargs.get('password', None)
+
+        if self._username is None:
+            raise ValueError("username argument is required!")
+        if self._account_name is None:
+            raise ValueError("account_name argument is required!")
+        if self._password is None:
+            raise ValueError("password argument is required!")
+        if type(self._username) is not str:
+            raise TypeError("username argument must be a string!")
+        if type(self._account_name) is not str:
+            raise TypeError("account_name argument must be a string!")
+        if type(self._password) is not str:
+            raise TypeError("password argument must be a string!")
+
         self._config_driver()
 
     def _config_driver(self) -> None:
-        pass
+        chrome_options = Options()
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument("--test-type")
+        chrome_options.add_argument("test-type")
+        chrome_options.add_argument("start-maximized")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--enable-precise-memory-info")
+        chrome_options.add_argument("--disable-popup-blocking")
+        chrome_options.add_argument("--disable-default-apps")
+        chrome_options.add_argument("test-type=browser")
+        chrome_options.add_argument("--incognito")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
+        chrome_options.add_experimental_option("mobileEmulation", {"deviceName": "iPhone 12 Pro"})
+
+        self._driver = webdriver.Chrome(ChromeDriverManager().install(),
+                            options=chrome_options)
 
     def _login(self) -> None:
-        pass
+        try:
+            self._driver.get("https://www.instagram.com/")
 
-    def _delete_post(self, post : Post) -> None:
-        pass
+            sleep(2)
+            self._driver.find_element(By.XPATH, "//div[contains(text(), 'Log in')]").click()
+            self._driver.find_element(By.XPATH, "//input[@name=\"username\"]").send_keys(self._username)
+            self._driver.find_element(By.XPATH, "//input[@name=\"password\"]").send_keys(os.getenv('TEST_INSTAGRAM_PASSWORD'))
+            self._driver.find_element(By.XPATH, '//button[@type="submit"]').click()
+            sleep(5)
+            try:
+                self._driver.find_element(By.XPATH, "//div[contains(text(), 'Not Now')]").click()
+                sleep(5)
+            except:
+                pass
+            try:
+                self._driver.find_element(By.XPATH, "//button[contains(text(), 'Not Now')]").click()
+                sleep(2)
+            except:
+                pass
+        except Exception as e:
+            raise Exception(e)
 
-    def post(post : Post) -> str:
-        pass
+    def _delete_post(self, url : str) -> None:
+        self._driver.get(url)
+        sleep(2)
+        self._driver.find_element(By.XPATH, "//*[name()='svg' and @aria-label='More options']").find_element(By.XPATH, "..").find_element(By.XPATH, "..").find_element(By.XPATH, "..").click()
+        sleep(1)
+        self._driver.find_element(By.XPATH, '//button[contains(text(), "Delete")]').click()
+        sleep(1)
+        self._driver.find_element(By.XPATH, '//button[contains(text(), "Delete")]').click()
+
+    def post(self, post : Post) -> str:
+        self._login()
+
+        self._driver.find_element(By.XPATH, "//a[contains(@href, '#')]").click()
+        sleep(5)
+        self._driver.find_element(By.XPATH, "//span[contains(text(), 'Post')]").click()
+        sleep(5)
+        self._driver.find_element(By.XPATH, '//input[@type="file"]').send_keys(post.filepath)
+        sleep(5)
+        try:
+            self._driver.find_element(By.XPATH, '//span[contains(text(), "Expand")]').click()
+            sleep(1)
+        except:
+            pass
+        self._driver.find_element(By.XPATH, "//button[contains(text(), 'Next')]").click()
+        sleep(5)
+        self._driver.find_element(By.XPATH, "//textarea[@aria-label='Write a caption…']").click()
+        sleep(1)
+        self._driver.find_element(By.XPATH, "//textarea[@aria-label='Write a caption…']").send_keys(post.description)
+        sleep(1)
+        self._driver.find_element(By.XPATH, "//button[contains(text(), 'Share')]").click()
+        sleep(5)
+        self._driver.find_element(By.XPATH, "//*[name()='svg' and @aria-label='More options']").find_element(By.XPATH, "..").find_element(By.XPATH, "..").find_element(By.XPATH, "..").click()
+        sleep(2)
+        url = self._driver.find_element(By.XPATH, "//button[contains(text(), 'Delete')]/following-sibling::button/a").get_attribute('href')
+        return url
--- tests/functional/test_functional_poster.py
+++ tests/functional/test_functional_poster.py
@@ -2,50 +2,59 @@ import os
 
 from datetime import datetime, timedelta
 from selenium.webdriver.common.by import By
+from time import sleep
 
 from instabot.post import Post
 from instabot.poster import Poster
 
+def test_functional_login():
+    # Given.
+    poster = Poster(username=os.getenv('TEST_INSTAGRAM_USERNAME'), password=os.getenv('TEST_INSTAGRAM_PASSWORD'), account_name=os.getenv('TEST_INSTAGRAM_ACCOUNT_NAME'))
+
+    # When.
+    poster._login()
+
+    # Then.
+    profile_link = poster._driver.find_element(By.XPATH, f"//a[contains(@href, '{poster._username}')]").get_attribute('href')
+    poster._driver.quit()
+    assert profile_link == f"https://www.instagram.com/{poster._username}/"
+    
+def test_functional_delete_post():
+    # Given.
+    poster = Poster(username=os.getenv('TEST_INSTAGRAM_USERNAME'), password=os.getenv('TEST_INSTAGRAM_PASSWORD'), account_name=os.getenv('TEST_INSTAGRAM_ACCOUNT_NAME'))
+    post = Post(filepath=os.getcwd() + '/tests/img/valid_test_img.png', description="This is a test description.", scheduled_time=(datetime.now() + timedelta(days=1)).strftime("%d/%m/%Y %H:%M:%S"))
+    url = poster.post(post)
+    default_message = "Sorry, this page isn't available."
+
+    # When.
+    poster._delete_post(url)
+
+    # Then.
+    poster._driver.get(url)
+    sleep(2)
+    poster._driver.get(url)
+    sleep(2)
+    received_message = poster._driver.find_element(By.XPATH, f"//span[contains(text(), 'Sorry, this page')]").text
+    poster._driver.quit()
+    assert received_message == default_message
+
 def test_functional_post_file():
     # Given.
-    poster = Poster()
-    scheduled_time = (datetime.now() + timedelta(days=1))
-    post = Post(filepath=os.getcwd() + '/tests/img/valid_test_img.png', description="This is a test description.", scheduled_time=scheduled_time.strftime("%d/%m/%Y %H:%M:%S"))
-
+    poster = Poster(username=os.getenv('TEST_INSTAGRAM_USERNAME'), password=os.getenv('TEST_INSTAGRAM_PASSWORD'), account_name=os.getenv('TEST_INSTAGRAM_ACCOUNT_NAME'))
+    post = Post(filepath=os.getcwd() + '/tests/img/valid_test_img.png', description="This is a test description.", scheduled_time=(datetime.now() + timedelta(days=1)).strftime("%d/%m/%Y %H:%M:%S"))
+    
     # When.
     url = poster.post(post)
 
     # Then.
     poster._driver.get(url)
-    post_description = poster._driver.find_element(By.XPATH, f"//div[contains(text(), '{post.description}')]").text
-    formatted_date = scheduled_time.strftime("%d %b, %Y")
-    post_image_information = poster._driver.find_element(By.XPATH, f"//img[contains(@alt, '{formatted_date}')]").text
+    sleep(2)
+    post_description = poster._driver.find_element(By.XPATH, f"//h1[contains(text(), '{post.description}')]").text
+    formatted_date = datetime.now().strftime("%b %d, %Y")
+    post_image_information = poster._driver.find_element(By.XPATH, f"//img[contains(@alt, '{formatted_date}')]").get_attribute("alt")
     
     assert post_description == post.description
     assert post_image_information == f"Photo by {poster._account_name} on {formatted_date}."
     
-    poster._delete_post(post)
-
-def test_functional_login():
-    # Given. When.
-    poster = Poster()
-
-    # Then.
-    profile_link = poster._driver.find_element(By.XPATH, f"//a[contains(@href, '{poster._username}')]").href
-    assert profile_link == f"/{poster._username}/"
-    
-
-def test_functional_delete_post():
-    # Given.
-    poster = Poster()
-    post = Post(filepath=os.getcwd() + '/tests/img/valid_test_img.png', description="This is a test description.", scheduled_time=(datetime.now() + timedelta(days=1)).strftime("%d/%m/%Y %H:%M:%S"))
-    url = poster.post(post)
-    default_message = "The link you followed may be broken, or the page may have been removed."
-
-    # When.
-    poster._delete_post(post)
-
-    # Then.
-    poster._driver.get(url)
-    received_message = poster._driver.find_element(By.XPATH, f"//span[contains(text(), '{default_message}')]").text
-    assert received_message == {default_message}
+    poster._delete_post(url)
+    poster._driver.quit()
